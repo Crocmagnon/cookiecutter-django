2064adb9e67c02a4bfe0188dee638dad7338daa1
Gabriel Augendre <gabriel@augendre.info>
2023-01-27 10:58:09 +0100

    Cleanup remove old names

--- {{cookiecutter.project_slug}}/src/common/models.py
+++ {{cookiecutter.project_slug}}/src/common/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    """Default custom user model for My Awesome Project."""
+    """Default custom user model for {{ cookiecutter.project_name }}."""
 
     pass
--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/urls.py
@@ -11,7 +11,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('{{cookiecutter.project_slug}}/', include('{{cookiecutter.project_slug}}.urls'))
 """
 from django.conf import settings
 from django.conf.urls.static import static
